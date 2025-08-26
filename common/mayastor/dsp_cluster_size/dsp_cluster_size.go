26dde17b79717d01600b4583b491aca4e58ad3f0
rohan2794 <rohan.kumar@mayadata.io>
2025-08-26 15:16:25 +0530

    feat: create cluster size disk pool
    Signed-off-by: rohan2794 <rohan.kumar@mayadata.io>

--- common/mayastor/dsp_cluster_size/dsp_cluster_size.go
+++ common/mayastor/dsp_cluster_size/dsp_cluster_size.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 
 	"github.com/openebs/openebs-e2e/common"
+	"github.com/openebs/openebs-e2e/common/custom_resources"
+	"github.com/openebs/openebs-e2e/common/custom_resources/types"
 	"github.com/openebs/openebs-e2e/common/e2e_config"
 	"github.com/openebs/openebs-e2e/common/k8stest"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
+
+var poolOnlineTimeoutSec = 120 // seconds
 
 func SetMayastorDspClusterSize(size, helmChart, helmRelease, helmVersion string) error {
 	var values map[string]interface{}
@@ -37,4 +41,60 @@ func SetMayastorDspClusterSize(size, helmChart, helmRelease, helmVersion string)
 	}
 
 	return nil
+}
+
+// CreateClusterSizePools creates cluster size pools
+// It takes a map of node, disk and cluster size as input and returns a map of pool name and pool object
+func CreateClusterSizePools(nodeDiskMap map[string]string, clusterSize string) (map[string]types.DiskPool, error) {
+	pools := make(map[string]types.DiskPool)
+	for node, disk := range nodeDiskMap {
+		poolName := "pool-" + node
+		pool, err := custom_resources.CreateMsPoolWithClusterSize(poolName, node, []string{disk}, clusterSize)
+		if err != nil {
+			return pools, err
+		}
+		pools[poolName] = pool
+	}
+	return pools, nil
+}
+
+// CreateAndWaitForClusterSizePools creates cluster size pools and waits for them to be online
+// It takes a map of node, disk and cluster size as input and returns a map of pool name and pool object
+// It returns an error if the pools are not online within the given time
+func CreateAndWaitForClusterSizePools(nodeDiskMap map[string]string, clusterSize string) (map[string]types.DiskPool, error) {
+	pools, err := CreateClusterSizePools(nodeDiskMap, clusterSize)
+	if err != nil {
+		return pools, err
+	}
+
+	// wait for all pools to be online
+	err = k8stest.WaitForPoolsToBeOnline(poolOnlineTimeoutSec)
+	if err != nil {
+		return pools, fmt.Errorf("all pools are not online, error: %v", err)
+	}
+
+	// refresh the pool list
+	poolList, err := custom_resources.ListMsPools()
+	if err != nil {
+		return pools, fmt.Errorf("failed to list pools, error: %v", err)
+	}
+
+	for _, pool := range poolList {
+		if pools[pool.GetName()] != nil {
+			pools[pool.GetName()] = pool
+
+		}
+	}
+
+	// check if all pools cluster size matches
+	for _, pool := range pools {
+		if pool.GetClusterSize() != clusterSize {
+			return pools, fmt.Errorf("pool %s cluster size %s does not match with provide size %s",
+				pool.GetName(),
+				pool.GetClusterSize(),
+				clusterSize,
+			)
+		}
+	}
+	return pools, nil
 }
