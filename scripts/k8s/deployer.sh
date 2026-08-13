a511824ea1bbac73738921e981735c593915449a
Akash Bc <akash.bc@datacore.com>
2026-08-13 06:57:10 +0000

    chore: update openebs chart version
    Signed-off-by: Akash Bc <akash.bc@datacore.com>

--- scripts/k8s/deployer.sh
+++ scripts/k8s/deployer.sh
@@ -193,8 +193,8 @@ EOF
     # Should already be installed by prereq script
     ZFS=$(realpath $(which zfs))
     cat <<EOF >>$TMP_KIND_ZFS
-    #/bin/sh
-    chroot /host $ZFS "\$@"
+#!/bin/sh
+chroot /host $ZFS "\$@"
 EOF
     chmod +x $TMP_KIND_ZFS
     cat <<EOF >> "$TMP_KIND_CONFIG"
@@ -211,7 +211,7 @@ EOF
     # Should already be installed by prereq script
     ZPOOL=$(realpath $(which zpool))
     cat <<EOF >>$TMP_KIND_ZPOOL
-    #/bin/sh
+    #!/bin/sh
     chroot /host $ZPOOL "\$@"
 EOF
     chmod +x $TMP_KIND_ZPOOL
@@ -224,12 +224,12 @@ EOF
       propagation: HostToContainer
 EOF
   fi
-  
+
   if [ "$SETUP_LVM" = "true" ]; then
     # Should already be installed by prereq script
     LVM=$(realpath $(which lvm))
     cat <<EOF >>$TMP_KIND_LVM
-    #/bin/sh
+    #!/bin/sh
     chroot /host $LVM "\$@"
 EOF
     chmod +x $TMP_KIND_LVM
