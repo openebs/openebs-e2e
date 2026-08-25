9cb3faa0c768aee28b4390d1671ff83946b4a224
Ranjith M P <92041707+r1jt@users.noreply.github.com>
2026-08-25 20:42:53 +0530

    fix:update image repo name for mayastor images
    Signed-off-by: Ranjith M P <92041707+r1jt@users.noreply.github.com>

--- common/controlplane/v1/upgrade.go
+++ common/controlplane/v1/upgrade.go
@@ -163,7 +163,7 @@ func buildUpgradeArgs(meta upgradeMetadata, opts upgradeOptions) []string {
 			cmdArgs = append(cmdArgs,
 				"--registry", detectedReg,
 				"--repo-namespace", regNS,
-				"--set", fmt.Sprintf("%s.registry=%s,%s.repo=%s", setPrefix, detectedReg, setPrefix, regNS),
+				"--set", fmt.Sprintf("%s.registry=%s,%s.repo=%s", setPrefix, detectedReg, setPrefix, common.MayastorRegistryNameSpace),
 			)
 		}
 		// empty string → DockerHub: plugin uses it by default, no --registry flag needed
